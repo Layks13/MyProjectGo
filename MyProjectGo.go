1de88d1c1d7d0fdb3dbbb68f807a4914a4b9f021
Layks13 <68061010+Layks13@users.noreply.github.com>
2023-07-19 23:48:12 +0300

    Fix

--- MyProjectGo.go
+++ MyProjectGo.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+var i, j int = 1, 2
+
 func main() {
-	fmt.Println("Hello, World!!")
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
 }
